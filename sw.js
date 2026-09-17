4aa1d11759f09105bcb3f3afdecd9fb70babb061
bluwateredh-droid <bluwateredh@gmail.com>
2026-09-17 16:26:59 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'frog-survivor-v11';
+const CACHE = 'frog-survivor-v12';
 const FILES = ['./','./index.html','./style.css','./game.js','./manifest.webmanifest','./icon.svg','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
